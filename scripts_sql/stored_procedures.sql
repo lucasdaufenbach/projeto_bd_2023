8f8c566fa438ea4858dd98340a4f0b517beab198
angielleal <145812726+angielleal@users.noreply.github.com>
2023-12-04 21:25:50 -0300

    Update stored_procedures.sql
    criação da stored procedure "sp_AtualizarStatusMesa"

--- scripts_sql/stored_procedures.sql
+++ scripts_sql/stored_procedures.sql
@@ -1,13 +1,10 @@
-DROP PROCEDURE IF EXISTS pr_imc
-GO
-
-CREATE PROCEDURE pr_imc (@massa numeric(10,2), @altura numeric(10,2), @resultado numeric(10,2) OUTPUT) AS
+CREATE PROCEDURE sp_AtualizarStatusMesa
+    @idmesa INT,
+    @novoStatus CHAR(1)
+AS
 BEGIN
-	DECLARE @altura_quadrado numeric(10,2)
+    UPDATE mesa
+    SET status = @novoStatus
+    WHERE idmesa = @idmesa;
+END;
 
-	SELECT @altura_quadrado = POWER(@altura, 2)
-
-	SELECT @resultado = @massa / @altura_quadrado 
-	
-END
-GO
