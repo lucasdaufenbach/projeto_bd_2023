dee784817b4be6e219e58ab13d71a096377f8cc5
angielleal <145812726+angielleal@users.noreply.github.com>
2023-12-04 21:27:41 -0300

    Update triggers.sql
    criação da trigger "trg_AuditoriaCliente"

--- scripts_sql/triggers.sql
+++ scripts_sql/triggers.sql
@@ -1,14 +1,36 @@
-DROP TRIGGER IF EXISTS td_apolice
-go
-
-CREATE TRIGGER td_apolice on apolice AFTER DELETE as
+CREATE TRIGGER trg_AuditoriaCliente
+ON cliente
+AFTER INSERT, UPDATE, DELETE
+AS
 BEGIN
-	IF ROWCOUNT_BIG() = 0
-		RETURN
+    IF OBJECT_ID('tempdb..#ClienteAudit') IS NOT NULL
+        DROP TABLE #ClienteAudit;
 
-	INSERT INTO apolice_log
-	SELECT *, GETDATE(), SUSER_NAME()
-	FROM deleted
+    CREATE TABLE #ClienteAudit (
+        Operacao    VARCHAR(10),
+        idcliente   INT,
+        nm_cliente  VARCHAR(50),
+        telefone    CHAR(11),
+        cpf         CHAR(11),
+        DataAlteracao DATETIME DEFAULT GETDATE()
+    );
 
-END
-GO
+    DECLARE @Operacao VARCHAR(10);
+    IF EXISTS(SELECT * FROM inserted)
+    BEGIN
+        IF EXISTS(SELECT * FROM deleted)
+            SET @Operacao = 'UPDATE';
+        ELSE
+            SET @Operacao = 'INSERT';
+
+        INSERT INTO #ClienteAudit (Operacao, idcliente, nm_cliente, telefone, cpf)
+        SELECT @Operacao, idcliente, nm_cliente, telefone, cpf FROM inserted;
+    END
+    ELSE IF EXISTS(SELECT * FROM deleted)
+    BEGIN
+        SET @Operacao = 'DELETE';
+        INSERT INTO #ClienteAudit (Operacao, idcliente, nm_cliente, telefone, cpf)
+        SELECT @Operacao, idcliente, nm_cliente, telefone, cpf FROM deleted;
+    END;
+    -- Aqui você pode realizar operações com a tabela de auditoria se desejar
+END;
