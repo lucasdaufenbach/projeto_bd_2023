99eddf1cdff9d88f4a46548427664eef32508222
marcoshjf <145942375+marcoshjf@users.noreply.github.com>
2023-12-04 21:01:44 -0300

    Update indices.sql

--- scripts_sql/indices.sql
+++ scripts_sql/indices.sql
@@ -1,2 +1,9 @@
-CREATE INDEX idx_placa ON carro (placa) INCLUDE (dt_apolice)
-GO
+--INDICES PARA AS CONSULTAS
+CREATE INDEX idx_comanda_idcliente ON comanda(idcliente);
+
+CREATE INDEX idx_pagamento_idcomanda ON pagamento(idcomanda);
+
+CREATE INDEX idx_comanda_idfuncionario ON comanda(idfuncionario);
+
+CREATE INDEX idx_pedidos_idcardapio ON pedidos(idcardapio);
+CREATE INDEX idx_pedidos_idpedido_idcardapio ON pedidos(idpedido, idcardapio);
